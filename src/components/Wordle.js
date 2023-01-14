7892d994a47c2b5553e5c2c42db2558a45e8c3d3
Vinayak <myselfvinayakhegde@gmail.com>
2023-01-14 14:38:09 +0530

    Created grid

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, turn, isCorrect } =
@@ -17,7 +18,9 @@ export default function Wordle({ solution }) {
   return (
     <div>
       <div>Solution - {solution.toUpperCase()}</div>
-      <div>Current guess - {currentGuess.toUpperCase()}</div>
+      <div>Current guess - {currentGuess.toUpperCase()}
+      </div>
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
     </div>
   );
 }
