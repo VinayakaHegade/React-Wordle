82c5a34ad41cbd598fe11fdf69bb388710d9d2c4
Vinayak <myselfvinayakhegde@gmail.com>
2023-01-14 15:36:51 +0530

    Styled and displayed word on grid

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export default function () {
+export default function ({ guess }) {
+  if (guess) {
+    return (
+      <div className="row past">
+        {guess.map((letterObj, index) => (
+          <div key={index} className={letterObj.color}>
+            {letterObj.key}
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="row">
       <div></div>
